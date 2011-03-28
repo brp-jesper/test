263db95c82be3f91db8d33644e29dfba0fd9fb6e
Jesper Wetterbrandt <jesper.wetterbrandt@brpsystems.se>
2011-03-28 20:49:12 +0200

    apa banan

---
+++ helloApp/grails-app/controllers/brp/ProductController.groovy
@@ -0,0 +1,22 @@
+package brp
+
+import helloapp.Product
+
+class ProductController {
+
+    def fibonacciService
+
+    def index = { redirect action:'list' }
+
+    def list = {
+
+      [products:Product.list()]}
+
+    def show = {
+        def product = Product.read(params.id as int)
+        [product: product]
+    }
+
+
+
+}
